0804acfd53a370bc4509674e7b70c060563e0a31
23021985 <h.de.vreugd@hotmail.com>
2021-06-24 19:21:01 +0200

    opdracht-compleet

--- src/main/java/behavioral/observer/AnalyticsService.java
+++ src/main/java/behavioral/observer/AnalyticsService.java
@@ -1,10 +1,10 @@
 package behavioral.observer;
 
-public class AnalyticsService {
+public class AnalyticsService implements InventoryObserver {
     private int numberOfEvents = 0;
 
-    // This method should be called when we I get notified by the inventoryService
-    private void applyAnalytics(String productName, int inventory) {
+    @Override
+    public void notify(String productName, int inventory) {
         System.out.printf("Inventory of product %s changed to %d", productName, inventory);
         numberOfEvents++;
     }
--- src/test/java/structural/adapter/AdapterTest.java
+++ src/test/java/structural/adapter/AdapterTest.java
@@ -1,5 +1,6 @@
 package structural.adapter;
 
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,10 +9,10 @@ public class AdapterTest {
 
     @Test
     public void itShouldExposeTheMeasurementServiceUsingTheAdapterPattern() {
-//        MeasurementService measurementService = new MeasurementServiceImpl();
-//        MetricMeasurementServiceAdapter adapter = new MetricMeasurementServiceAdapterImpl(measurementService);
-//
-//        assertThat(adapter.calculateRectangleCircumferenceInCm(10, 10)).isCloseTo(40, Offset.offset(0.1));
-//        assertThat(adapter.calculateRectangleSurfaceInCm(10, 10)).isCloseTo(100, Offset.offset(0.1));
+        MeasurementService measurementService = new MeasurementServiceImpl();
+        MetricMeasurementServiceAdapter adapter = new MetricMeasurementServiceAdapterImpl(measurementService);
+
+        assertThat(adapter.calculateRectangleCircumferenceInCm(10, 10)).isCloseTo(40, Offset.offset(0.1));
+        assertThat(adapter.calculateRectangleSurfaceInCm(10, 10)).isCloseTo(100, Offset.offset(0.1));
     }
 }
